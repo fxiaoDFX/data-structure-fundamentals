585599f9d114e5176dd731dd7eeef608fb37938b
Frankie Xiao <fxiaoX089@live.com>
2023-01-14 17:08:06 -0600

    Finish implementation of BST methods

--- javascript/binary-search-trees.js
+++ javascript/binary-search-trees.js
@@ -42,24 +42,155 @@ class BST {
         return root
     }
 
+    delete(data) {
+        return this.#delete(data, this.root)
+    }
+
+    #delete(data, node) {
+        if (!node) return null
+
+        if (data < node.data) node.left = this.#delete(data, node.left)
+        if (data > node.data) node.right = this.#delete(data, node.right)
+        if (data === node.data) {
+            node = this.#getSuccessor(node)
+        }
+        return node
+    }
+
+    #getSuccessor(node) {
+        if (node.left && node.right) {
+            const successor = this.#getMin(node.right)
+            node.data = successor.data
+            node.right = this.#delete(successor.data, node.right)
+            return node
+        } else {
+            const successor = node.left || node.right
+            node = null
+            return successor
+        }
+    }
+
+    #getMin(node) {
+        if (!node.left) return node
+        return this.#getMin(node.left)
+    }
+
     find(value, node = this.root) {
-        if (!node) return "Node not found"
+        if (!node) return null
         if (value === node.data) return node
 
         if (value < node.data) return this.find(value, node.left)
         else return this.find(value, node.right)
     }
 
-    levelOrder() {
+    levelOrder(cb) {
         const queue = [this.root]
 
+        const bfsList = []
         while (queue.length) {
             const node = queue.shift()
 
             if (node.left) queue.push(node.left)
             if (node.right) queue.push(node.right)
-            console.log(node)
+
+            if (cb) {
+                cb(node)
+            } else {
+                bfsList.push(node)
+            }
         }
+        if (bfsList.length > 0) return bfsList
+    }
+
+    inorder(cb, node = this.root, list = []) {
+        if (!node) return
+
+        this.inorder(cb, node.left, list)
+        if (cb) {
+            cb(node)
+        } else {
+            list.push(node)
+        }
+        this.inorder(cb, node.right, list)
+
+        if (list.length > 0) return list
+    }
+
+    preorder(cb, node = this.root, list = []) {
+        if (!node) return
+
+        if (cb) cb(node)
+        else list.push(node)
+        this.preorder(cb, node.left, list)
+        this.preorder(cb, node.right, list)
+
+        if (list.length > 0) return list
+    }
+
+    postorder(cb, node = this.root, list = []) {
+        if (!node) return
+
+        this.postorder(cb, node.left, list)
+        this.postorder(cb, node.right, list)
+        if (cb) cb(node)
+        else list.push(node)
+
+        if (list.length > 0) return list
+    }
+
+    height(node) {
+        if (typeof node === "number") node = new Node(node)
+        return this.find(node.data)
+            ? this.#height(this.find(node.data)) - 1
+            : null
+    }
+
+    #height(node) {
+        if (!node) return 0
+
+        const leftHeight = this.#height(node.left)
+        const rightHeight = this.#height(node.right)
+        return Math.max(leftHeight, rightHeight) + 1
+    }
+
+    depth(node) {
+        if (typeof node === "number") node = new Node(node)
+        return this.find(node.data) ? this.#depth(node) : null
+    }
+
+    #depth(node, root = this.root) {
+        if (!node || !root) return null
+        if (node.data === root.data) return 0
+
+        if (node.data < root.data) return this.#depth(node, root.left) + 1
+        else return this.#depth(node, root.right) + 1
+    }
+
+    isBalanced() {
+        if (!this.root) return null
+
+        let balanced = true
+        this.levelOrder((node) => {
+            let leftSubTreeHeight = node.left ? this.height(node.left) : -1
+            let rightSubTreeHeight = node.right ? this.height(node.right) : -1
+
+            if (Math.abs(leftSubTreeHeight - rightSubTreeHeight) > 1) {
+                balanced = false
+            }
+        })
+        return balanced
+    }
+
+    rebalance() {
+        if (!this.root) return null
+
+        let sorted = []
+        this.inorder((node) => {
+            sorted.push(node.data)
+        })
+
+        this.root = this.buildTree(sorted)
+        console.log(sorted)
     }
 
     prettyPrint(node = this.root, prefix = "", isLeft = true) {
@@ -82,14 +213,20 @@ class BST {
 }
 
 const test = (() => {
-    const array = [1, 0, 1, 2, 3, 7, 5, 3]
-    console.log([...new Set(array)].sort((a, b) => a - b))
+    const array = [1, 0, 1, 2, 3, 7, 5, 3, 12, 11, 4]
+    const array2 = [10, 3, 4]
     const tree = new BST(array)
+    const test = new BST(array2)
     tree.insert(10)
-    tree.insert(-1)
     tree.insert(0)
+    test.insert(-1)
+    test.insert(-3)
     tree.prettyPrint()
-    tree.levelOrder()
+    console.log(tree.delete(11))
+    tree.prettyPrint()
+    tree.rebalance()
+    tree.prettyPrint()
+    console.log(tree.isBalanced())
 })()
 
 export default test
